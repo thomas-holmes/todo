76bf405cea7ea0a81e94ff8b267dbe8512c95fb1
Thomas Holmes <thomas@devminded.com>
2013-04-15 00:52:47 -0400

    Add task resource/controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Todo::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :tasks
   
   root :to => 'application#index'
 
